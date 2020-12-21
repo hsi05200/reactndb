db315c11b7b0118512ee2f43c1d41c02067dc43d
stchwang <hsi05200@gmail.com>
2020-12-22 00:03:47 +0900

    Material UI를 활용한 리스트 CSS 설정

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,47 +1,21 @@
 import { Component } from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends Component {
 
   render() {
 
     return (
-      <div style={{display:'flex', margin:'10px 0'}}>        
-        <CustomerProfile
-          id={this.props.id}
-          image={this.props.image}  
-          name={this.props.name}
-        />
-        <CustomerInfo
-          birthday={this.props.birthday}
-          gender={this.props.gender}
-          job={this.props.job}
-        />        
-      </div>
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
     )
-  }
-}
-
-class CustomerProfile extends Component {
-  render() {
-    return (
-      <div style={{display:'flex', alignItems:'center'}}>
-        <p>{this.props.id}</p>        
-        <img src={this.props.image} alt="profile"/>
-        <p>{this.props.name}</p>
-      </div>
-    );    
-  }
-}
-
-class CustomerInfo extends Component {
-  render() {
-    return (
-      <div style={{display:'flex', alignItems:'center'}}>
-        <p>{this.props.birthday}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.job}</p>
-      </div>
-    )    
   }
 }
 
