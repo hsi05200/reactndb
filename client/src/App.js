84777b6fb876a3aad2d2b664380f5df80d0d55dd
stchwang <hsi05200@gmail.com>
2020-12-23 00:49:42 +0900

    서버에 연동할 App.js 코딩작업

--- client/src/App.js
+++ client/src/App.js
@@ -22,36 +22,27 @@ import { withStyles } from '@material-ui/core/styles';
     }
   })
 
-  const customer = [
-    {
-      id: 1,
-      image: 'https://placeimg.com/50/50/1',
-      name:'홍길동',
-      birthday: '961222',
-      gender: '남자',
-      job: '대학생'
-    },
-    {
-      id: 2,
-      image: 'https://placeimg.com/50/50/2',
-      name:'사임당',
-      birthday: '921011',
-      gender: '여자',
-      job: '회사원'
-    },
-    {
-      id: 3,
-      image: 'https://placeimg.com/50/50/3',
-      name:'일지매',
-      birthday: '930218',
-      gender: '남자',
-      job: '학원강사'
-    }
-  ]
+  
 
 class App extends Component {   
-  render() {
 
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render() {
     const { classes } = this.props;
     return (         
         <Paper className={classes.root}>
@@ -67,19 +58,20 @@ class App extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-              {
-                customer.map(c => {
-                  return (                
-                    <Customer
-                      id={c.id}
-                      image={c.image}
-                      name={c.name}
-                      birthday={c.birthday}
-                      gender={c.gender}
-                      job={c.job}            
-                    />                
+              { this.state.customers 
+                ? this.state.customers.map(c => {
+                    return (                
+                      <Customer
+                        id={c.id}
+                        image={c.image}
+                        name={c.name}
+                        birthday={c.birthday}
+                        gender={c.gender}
+                        job={c.job}            
+                      />                
                   );
-                })                         
+                })
+                : ""                         
               }
             </TableBody>
           </Table>
