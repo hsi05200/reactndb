2c9994117c89b8dde7ef009745cd699bcf758b50
stchwang <hsi05200@gmail.com>
2020-12-27 02:31:52 +0900

    모달을 활용한 삭제 경고메세지를 통해 삭제와 취소를 구현

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import {post} from 'axios'; // 백엔더와 프론트엔더와 상호 통신을 위한 라이버리설치
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  hidden: {
+    display:'none'
+  }
+})
 
 class CustomerAdd extends React.Component {
 
@@ -11,7 +24,8 @@ class CustomerAdd extends React.Component {
       birthday:'',
       gender:'',
       job:'',
-      fileName:'',        
+      fileName:'',
+      open: false
     }
   }
 
@@ -29,6 +43,7 @@ class CustomerAdd extends React.Component {
       gender:'',
       job:'',
       fileName:'',
+      open: false
     })    
   }
 
@@ -61,61 +76,96 @@ class CustomerAdd extends React.Component {
     return post(url, formData, config);
   }
 
+  handleClickOpen =() => {
+    this.setState({
+      open: true
+    });
+  }
+
+  handleClose =() => {
+    this.setState({      
+        file: null,
+        userName:'',
+        birthday:'',
+        gender:'',
+        job:'',
+        fileName:'',
+        open: false
+      });    
+  }
+
   render() {
+    const { classes } = this.props;
     return (
-      <form className="customerForm" onSubmit={this.handleFormSubmit}>
-        <h1>고객추가</h1>        
-        <p>
-          <label style={{marginRight:15}}>프로필 이미지</label>
-          <input type="file"
-                  name="file"
-                  file={this.state.file}
-                  value={this.state.fileName}
-                  onChange={this.handleFileChange}
-          />
-        </p>
-
-        <div>
-          <p>
-            <label>이름</label>
-            <input type="text"
-                  name="userName"
-                  value={this.state.userName}
-                  onChange={this.handleValueChange}
-            />
-          </p>
-          <p>
-            <label>생년월일</label>
-            <input type="text"
-                  name="birthday"
-                  value={this.state.birthday}
-                  onChange={this.handleValueChange}
-            />
-          </p>
+      <div>
+        <div style={{display:'flex', justifyContent:'space-between', marginBottom:-25, borderBottom:'3px solid #ddd'}}>
+          <h1 style={{marginLeft:15}}>고객관리</h1>
+          <Button variant="contained"
+                  color="primary"
+                  onClick={() => this.handleClickOpen()}
+                  style={{margin:'21px 15px 0', height:40}}                
+                  >
+            고객 추가 하기
+          </Button>
         </div>
-
-        <div>
-          <p>
-            <label>성별</label>
-            <input type="text"
-                  name="gender"
-                  value={this.state.gender}
-                  onChange={this.handleValueChange}
-            />
-          </p>
-          <p>
-            <label>직업</label>
-            <input type="text"
-                  name="job"
-                  value={this.state.job}
-                  onChange={this.handleValueChange}
-            />
-          </p>
-        </div>
-        <button className="CtmSubmitBtn" type="submit">추가하기</button>        
-      </form>
+        <Dialog open={this.state.open}>
+          <DialogTitle style={{margin:'-20px 0 -50px'}}><h3>고객추가</h3></DialogTitle>
+          <DialogContent>
+            <p>              
+              <input type="file"                     
+                     file={this.state.file}
+                     value={this.state.fileName}
+                     onChange={this.handleFileChange}
+                     className={classes.hidden}
+                     accept="image/*"
+                     id="raised-button-file"
+              />
+              <label htmlFor="raised-button-file">
+                <Button variant="contained" color="primary" component="span" name="file" style={{width:'100%'}}>
+                    {this.state.fileName === ""
+                      ? "프로필 이미지 선택"
+                      : this.state.fileName
+                    }
+                </Button>
+              </label>
+            </p>
+            <p>
+              <TextField label="이름" type="text"
+                    name="userName"
+                    value={this.state.userName}
+                    onChange={this.handleValueChange}
+              />
+            </p>
+            <p>
+              <TextField label="생년월일" type="text"
+                    name="birthday"
+                    value={this.state.birthday}
+                    onChange={this.handleValueChange}
+              />
+            </p>
+            <p>
+              <TextField label="성별" type="text"
+                    name="gender"
+                    value={this.state.gender}
+                    onChange={this.handleValueChange}
+              />
+            </p>
+            <p>
+              <TextField label="직업" type="text"
+                    name="job"
+                    value={this.state.job}
+                    onChange={this.handleValueChange}
+              /> 
+            </p>         
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" color="primary" onClick={this.handleFormSubmit} style={{width:'70%'}}>추가</Button>
+            <Button variant="outlined" color="primary" onClick={this.handleClose} style={{width:'30%'}}>닫기</Button>
+          </DialogActions>
+        </Dialog>
+      </div>      
     );
   }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import  Typography from '@material-ui/core/Typography';
 
 
 class CustomerDelete extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    } 
+  }
+
+  handleClickOpen =() => {
+    this.setState({
+      open: true
+    });
+  }
+
+  handleClose =() => {
+    this.setState({              
+        open: false
+      });    
+  }
 
   deletedCustomer(id) {    
     const url = '/api/customers/' + id;
@@ -13,7 +38,39 @@ class CustomerDelete extends React.Component {
   render() {
 
     return (
-      <button onClick={(e) => {this.deletedCustomer(this.props.id)}}>삭제</button>
+      <div>
+        <Button 
+          variant="contained"
+          color="secondary"
+          onClick={this.handleClickOpen}>삭제</Button>
+        <Dialog open={this.state.open}>
+          <DialogTitle onClose={this.handleClose} style={{justifyContent:'center', display:'flex'}}>
+            <span style={{fontSize:18, fontWeight:600, color:'red'}}>
+              [ 삭제경고!! ]
+            </span>
+          </DialogTitle>
+          <DialogContent>
+            <Typography gutterBottom style={{textAlign:'center', marginBottom:-5}}>
+              <span style={{fontSize:13}}>
+                선택한 고객정보가 삭제됩니다!<br />
+                그래도 삭제하시겠습니까?
+              </span>
+            </Typography>
+          </DialogContent>
+          <DialogActions style={{justifyContent:'center'}}>
+            <Button variant="contained"
+                    color="primary"
+                    onClick={(e) => {this.deletedCustomer(this.props.id)}}>
+              삭제
+            </Button>
+            <Button variant="contained"
+                    color="primary"
+                    onClick={this.handleClose}>
+              취소
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     );
   }
 }
